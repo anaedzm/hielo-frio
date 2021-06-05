6b80d444966ebbc77105e6ad8894873ceed72fec
unknown <Ana Daz>
2021-06-04 23:57:15 -0500

    first

---
+++ src/assets/js/app.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+window.addEventListener('load', () => {
+
+  const body = document.querySelector('body');
+
+  function randomColor() {
+    axios.get(api).then(res => {
+      let color = res.data.colors[0].hex;
+
+      if (!color) {
+        console.error('Random color could not be fetched.');
+      }
+
+      color = '#' + color;
+
+      body.style.backgroundColor = color;
+    }).catch(() => console.error('Random color could not be fetched.'));
+  }
+
+  
+
+});
